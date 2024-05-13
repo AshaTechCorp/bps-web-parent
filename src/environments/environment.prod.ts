0ead80fc12b0ef3ba928864d87b4080738dd4642
Anon Thamcharoen <anonnon3952@gmail.com>
2024-05-13 12:13:17 +0700

    setup env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
 	production: true,
 	msalConfig: {
 		auth: {
-			clientId: 'ENTER_CLIENT_ID',
-			authority: 'ENTER_AUTHORITY'
+			clientId: '21a98560-0869-45fe-8d77-b4885165c0bf',
+			authority: 'https://login.microsoftonline.com/9732db31-8695-4030-ae01-e3217b1daaec'
 		}
 	},
 	apiConfig: {
-		scopes: ['ENTER_SCOPE'],
-		uri: 'ENTER_URI'
+		scopes: ['user.read'],
+		uri: 'https://graph.microsoft.com/v1.0/me'
 	}
 };
