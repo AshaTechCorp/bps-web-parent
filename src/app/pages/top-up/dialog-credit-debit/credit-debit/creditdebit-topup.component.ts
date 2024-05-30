ab99bf71bc9dcb5d96989e2c14262aa289d8a7be
PpsnPks <pipu8546@gmail.com>
2024-05-30 16:01:52 +0700

    ปุ่ม-complete

--- src/app/pages/top-up/dialog-credit-debit/credit-debit/creditdebit-topup.component.ts
+++ src/app/pages/top-up/dialog-credit-debit/credit-debit/creditdebit-topup.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule, DOCUMENT } from '@angular/common';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ElementRef, Renderer2, ViewChild, AfterViewInit, NgZone, inject, Inject, AfterViewChecked } from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ElementRef, Renderer2, NgZone, inject} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import { MatIconModule } from '@angular/material/icon';
@@ -38,18 +38,8 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
     templateUrl: './creditdebit-topup.component.html',
     changeDetection: ChangeDetectionStrategy.Default,
 })
-export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
-    //htmlStr: string = `<script type="text/typescript"
-    //                        src="https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js"
-    //                        data-apikey="pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB"
-    //                        data-amount="100.00"
-    //                        data-currency="THB"
-    //                        data-payment-methods="card"
-    //                        data-name="BANGKOK PATANA SCHOOL"
-    //                        data-mid="401012208444001">
-    //                    </script>`
-    //@ViewChild('formElement') formElement!: ElementRef;
-    //private renderer: Renderer2 = inject(Renderer2);
+export class CreditdebitTopupComponent implements OnInit{
+
     form: FormGroup;
     users: any[] = []
 	card: any
@@ -64,11 +54,6 @@ export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
         private _router: Router,
         private _topup: TopUpService,
         private activityroute: ActivatedRoute,
-        private ngZone: NgZone,
-        private el: ElementRef,
-        private renderer: Renderer2,
-        private sanitizer: DomSanitizer
-        //private ngZone: NgZone,
     ) {
         this.sn = this.decodeBase64(this.activityroute.snapshot.params['sn'])
         this.form = this._fb.group({
@@ -76,25 +61,6 @@ export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
         })
     }
     ngOnInit(): void {
-        
-             //this.ngZone.runOutsideAngular(() => {
-            //this.formElement.nativeElement.appendChild(script);
-        //});
-        //const script = this.renderer.createElement('script');
-        //script.onload = this.loadNextScript.bind(this);
-        //script.type = 'text/javascript';
-        //script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-        //script.setAttribute('data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-        //script.setAttribute('data-amount', '100.00');
-        //script.setAttribute('data-currency', 'THB');
-        //script.setAttribute('data-payment-methods', 'card');
-        //script.setAttribute('data-name', 'BANGKOK PATANA SCHOOL');
-        //script.setAttribute('data-mid', '401012208444001');
-
-        //const formElement = this.el.nativeElement.querySelector('#kbankGate');
-        //this.renderer.appendChild(formElement, script);
-        //this.renderer.appendChild(this._document.body, script);
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~use~~~~~~~~~~~~~~~~~~~~~~~
 		this._topup.get_card_by_SN(123123213).subscribe((resp: any) =>{
             this.card = {
                 id: resp.sn, 
@@ -108,91 +74,6 @@ export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
         })
     }
 
-    //loadNextScript() {
-    //    const scriptUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js');
-    //    console.log(scriptUrl as string);
-        
-    //    const script = this.renderer.createElement('script');
-    //    script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-    //    script.type = 'text/javascript';
-    
-    //    // กำหนดค่า data-* attributes อย่างถูกต้อง
-    //    script.setAttribute('data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-    //    script.setAttribute('data-amount', '100.00');
-    //    script.setAttribute('data-currency', 'THB');
-    //    script.setAttribute('data-payment-methods', 'card');
-    //    script.setAttribute('data-name', 'BANGKOK PATANA SCHOOL');
-    //    script.setAttribute('data-mid', '401012208444001');
-    //    //this.renderer.appendChild(this._document.body, script);
-    
-    //    // เพิ่มสคริปต์ไปที่ DOM
-    //    const formElement = this.el.nativeElement.querySelector('#kbankGate');
-    //    this.renderer.appendChild(formElement, script);
-    //}
-
-    //loadScript() {
-    //    const script = this.renderer.createElement('script');
-    //    script.type = 'text/javascript';
-    //    script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-    //    script.onload = () => {
-    //        console.log('Script loaded successfully');
-    //    };
-    //    script.onerror = (error: any) => {
-    //    console.error('Script load error', error);
-    //    };
-    //    this.renderer.setAttribute(script, 'data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-    //    this.renderer.setAttribute(script, 'data-amount', '100.00');
-    //    this.renderer.setAttribute(script, 'data-currency', 'THB');
-    //    this.renderer.setAttribute(script, 'data-payment-methods', 'card');
-    //    this.renderer.setAttribute(script, 'data-name', 'BANGKOK PATANA SCHOOL');
-    //    this.renderer.setAttribute(script, 'data-mid', '401012208444001');
-    //    //this.renderer.appendChild(this.formElement.nativeElement, script);
-    //    //this.renderer.appendChild(this.formElement.nativeElement, script);
-    //}
-
-    //loadScript() {
-    //    const script = this.renderer.createElement('script');
-    //    script.type = 'text/javascript';
-    //    script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-    //    this.renderer.setAttribute(script, 'data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-    //    this.renderer.setAttribute(script, 'data-amount', '100.00');
-    //    this.renderer.setAttribute(script, 'data-currency', 'THB');
-    //    this.renderer.setAttribute(script, 'data-payment-methods', 'card');
-    //    this.renderer.setAttribute(script, 'data-name', 'BANGKOK PATANA SCHOOL');
-    //    this.renderer.setAttribute(script, 'data-mid', '401012208444001');
-    //    this.renderer.appendChild(this.formElement.nativeElement, script);
-    //  }
-
-    ngAfterViewInit(): void {
-        this.ngZone.run(() => {
-            // โค้ดสำหรับโหลด KPayment script
-            const script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-            script.setAttribute('data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-            script.setAttribute('data-amount', '100.00');
-            script.setAttribute('data-currency', 'THB');
-            script.setAttribute('data-payment-methods', 'card');
-            script.setAttribute('data-name', 'BANGKOK PATANA SCHOOL');
-            script.setAttribute('data-mid', '401012208444001');
-            document.body.appendChild(script);
-            //document.getElementById('formElement')!.appendChild(script);
-        });
-    //    //this.loadScript();
-    //    //this.ngZone.runOutsideAngular(() => {
-    //        const script = document.createElement('script');
-    //        script.type = 'text/javascript';
-    //        script.src = 'https://kpaymentgateway.kasikornbank.com/ui/v2/kpayment.min.js';
-    //        script.setAttribute('data-apikey', 'pkey_prod_9929s93IlIbSmViDgW4eqjcqlnbT3JOWVrXB');
-    //        script.setAttribute('data-amount', '100.00');
-    //        script.setAttribute('data-currency', 'THB');
-    //        script.setAttribute('data-payment-methods', 'card');
-    //        script.setAttribute('data-name', 'BANGKOK PATANA SCHOOL');
-    //        script.setAttribute('data-mid', '401012208444001');
-    //        this.formElement.nativeElement.appendChild(script);
-    //    //});
-    }
-
     decodeBase64(input: string): string {
         return atob(input);
     }
@@ -203,13 +84,13 @@ export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
 
 	openDialogEdit(item: any) {
 		console.log(item);
-
+        item = this.form.value.amount
         const DialogRef = this.dialog.open(CreditDebitDialog, {
             disableClose: true,
-            width: '400px',
-            height: '417px',
+            width: 'fit-content',
+            height: 'fit-content',
             data: {
-                type: 'EDIT',
+                card: this.card,
                 value: item
             }
         });
@@ -275,24 +156,6 @@ export class CreditdebitTopupComponent implements OnInit, AfterViewInit{
 	backto(){
 		this._router.navigate(['/top-up',this.encodeBase64(this.sn)])
 	}
-
-    openDialog(): void {
-        const dialogRef = this.dialog.open(DialogComponent, {
-        disableClose: true,
-        width: '118px',
-        height: '118px',
-        });
-
-        setTimeout(() => {
-        dialogRef.close();
-        }, 3000);
-
-
-        dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed with result:', result);
-        this._router.navigate(['/select'])
-        });
-    }
 
     nextto(){       
         this._topup.setTopUp(+this.form.value.amount)
